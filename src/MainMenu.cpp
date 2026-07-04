b80fab78060c0a52070102c5140addd7c3c49496
zabidenhtf <policyaskrimerov@gmail.com>
2026-07-04 17:06:37 +0300

    Updated best scores table and about sections

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -19,29 +19,36 @@ int CMainMenu::Run(){
         scanf("%d", &Selector);
         switch(Selector){
         default:
-            printf("------------------------------\n");
-            printf("This option didn't exist\n");
-            printf("------------------------------\n");
+            printf("|------------------------------\n");
+            printf("|This option didn't exist\n");
+            printf("|------------------------------\n");
             return 1;
         case 1:
             GameCore.Run();
             return 1;
         case 2:
-            printf("------------------------------------------------------------\n");
-            printf("This program made by Mykyta Polishyk also known as ZABIDENTW\n");
-            printf("And this program licensed under MIT license\n");
-            printf("Current version: 1.1.0-trunk\n");
-            printf("------------------------------------------------------------\n");
+            printf("|------------------------------------------------------------|\n");
+            printf("|                      -= About =-                           |\n");
+            printf("|------------------------------------------------------------|\n");
+            printf("|This program made by Mykyta Polishyk also known as ZABIDENTW|\n");
+            printf("|And this program licensed under MIT license                 |\n");
+            printf("|Current version: 1.1.0-trunk                                |\n");
+            printf("|------------------------------------------------------------|\n");
             return 1;
         case 3:
-            printf("-------------------------------\n");
-            printf("           Best scores\n");
-            printf("-------------------------------\n");
-            printf("[Player name][Score][Round]\n");
+            printf("|------------------------------------------------------|\n");
+            printf("|                  -= Best scores =-                   |\n");
+            printf("|------------------------------------------------------|\n");
+            printf("| %-5s | %-24s | %-7s | %-7s |\n", "Place", "Player name", "Score", "Round");
+
             for (int i = 0; i < 8; i++){
-                printf("[%s][%d][%d]\n", Scores[i].Name, Scores[i].Score, Scores[i].Round);
+                printf("| %-5d | %-24s | %-7d | %-7d |\n",
+                       i + 1,
+                       Scores[i].Name,
+                       Scores[i].Score,
+                       Scores[i].Round);
             }
-            printf("-------------------------------\n");
+            printf("|------------------------------------------------------|\n");
             return 1;
         case 4:
             printf("Are you sure to quit? All of your progress will reset (Y/N)\n");
--- src/Types.hpp
+++ src/Types.hpp
@@ -1,6 +1,7 @@
 /* Copyright (C) 2026 Mykyta Polishyk */
 /* This project is licensed under the MIT license. */
 /* See the LICENSE file for details. */
+#pragma once
 
 struct BestScore{
     char Name[32];
